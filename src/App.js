592145bb8478320116aa1896de9c02026a80d6b3
DESKTOP-30QV8MR\sdukale <dukale.sachin@gmail.com>
2022-01-13 23:44:15 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import './App.css';
 import { useState } from 'react';
 import Question from './containers/question/Question';
-import { Button, Transition, Modal } from 'semantic-ui-react';
+import { Button, Transition, Modal, Divider } from 'semantic-ui-react';
 function App() {
 
   const questions = [
     {
       questionHeader: "You are locked up in a room and you have 4 digit pin to unlock. Solve this riddle",
-      questionText: "We are all part of this entity and the entity started its most popular social media around this pin. To get the pin, first decode which entity we are talking about and then take a look at their earliest post.",
+      questionText: "We are all part of this entity and the entity started its most popular social media around this year. To get the number, first decode which entity we are talking about and then take a look at their earliest post.",
       answer: "2017",
-      clue: "Instagram account of the university",
-      image: "src\images\riddler.png"
+      clue: "First post by instagram account of the university"
     },
     {
       questionHeader: "You have entered our world 'We fight. That is how we win, and that is how we die'",
-      questionText: "Only I can help you get out alive but only if you say my name. Solve this riddle",
+      questionText: "I am the king! I can help you get out alive but only if you say my name.",
       answer: "Ragnar",
-      clue: "Vikings Main character"
+      clue: "Vikings series Main character"
     },
     {
       questionHeader: "Good job !! Moving on! Now you are onboard on the vikings ship in the middle of the ocean. ",
       questionText: "I am part of atlantic ocean. But if you visit me conspiracies say you might disapper. Who am I ? ",
       answer: "Bermuda triangle",
-      clue: "I am also known as devil's triangle /_\ '"
+      clue: "I am also known as devil's triangle"
     },
     {
       questionHeader: "You have conquered the ocean and stranded on a deserted island.",
@@ -32,9 +31,9 @@ function App() {
     },
     {
       questionHeader: "Hurray! You have survived the deserted island! There is a door in front of you and it has blocked by spell and in order to break that spell you need to solve this mathematical equation. Mathematical equation to break the spell! Strange no?",
-      questionText: "8+(2*4)/4+19-8*(6/3)+4*2/8",
+      questionText: "(8+2*4/4+19-8*6/3+4*2/8)",
       answer: "14",
-      clue: "BODMAS rule",
+      clue: "use BODMAS rule",
     }
   ];
 
@@ -44,18 +43,23 @@ function App() {
   const [visible, setVisible] = useState(false);
   const [inputText, setinputText] = useState('');
   const [finish, setFinish] = useState(false);
+  const [duration,setDuration] = useState(10);
 
   const handleSubmitAnswer = () => {
-    if (qId != 4) {
+
+    if (qId <= 3) {
       if (inputText == question.answer || inputText == question.answer.toLocaleLowerCase()) {
         let id = qId + 1;
         setqId(id);
         setQuestion(questions[id]);
         setinputText('');
+        setDuration(10);
+        console.log(qId);
+    
       }
     }
-    else {
-      handleFinish();
+    else if(qId == 4){
+      if (inputText == question.answer) handleFinish();
     }
   }
 
@@ -73,28 +77,38 @@ function App() {
       <div className="ui container">
         {!visible &&
           <>
-            <h1>Hello there!</h1>
+            <h1 className="helloHeader">Hello there!</h1>
             <Button
               className="ui secondary"
               content="Enter into the escape room"
               onClick={toggleVisibility}
               size="large"
             />
+            <div className="item rule">
+              <h1 className="ruleHead">Rules</h1>
+              <Divider/>
+              <h2>1. There are 5 quizes are to be solved </h2>
+              <Divider/>
+              <h2>2. the next quiz will be unlocked only when you solve the current one. </h2>
+              <Divider/>
+              <h2>3. There is a time limit of 15 minutes to escape the room.</h2>
+              <Divider/>
+              <h2>4. When in doubt, ask for clue!</h2>
+            </div>
           </>}
 
         {!finish &&
           <Transition visible={visible} animation='drop' duration={400}>
             <div>
-              <Question question={question} handleSubmitAnswer={handleSubmitAnswer} handleInput={handleInput} inputText={inputText} />
+              <Question question={question} handleSubmitAnswer={handleSubmitAnswer} handleInput={handleInput} inputText={inputText} duration={duration} timeUp={false}/>
             </div>
           </Transition>}
 
-        {finish && <Transition visible={finish} animation='drop' duration={500}>
-          <div>
-          <h1 className="successMsg">You have successfully escaped from the room! We are glad that you make it!</h1>
-          </div>
+        <Transition visible={finish} animation='drop' duration={500}>
+
+          <h1 className="item">You have successfully escaped from the room! We are glad that you make it!</h1>
         </Transition>
-        }
+
 
 
       </div>
--- src/containers/question/Question.js
+++ src/containers/question/Question.js
@@ -1,46 +1,79 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Item } from "semantic-ui-react";
 import './Question.css';
-import { Button, Divider, Image, Transition, Modal } from 'semantic-ui-react'
+import { Button, Divider, Image, Transition, Modal } from 'semantic-ui-react';
+import questionImg from '../../images/digital.jpg';
+
 export default function Question(props) {
 
   const [clue, openClue] = React.useState(false);
+  const [timer, setTimer] = React.useState();
+  const [timeUp,setTimeUp] = React.useState(props.timeUp);
+  const [duration,setDuration] = React.useState(props.duration);
+
+  
+  function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.textContent = minutes + ":" + seconds;
+
+      if (--timer < 0) {
+        timer = duration;
+        setTimeUp(true);
+      }
+    }, 1000);
+  }
+  useEffect(() => {
+    startTimer(900, document.getElementById('counter'))
+    
+  }, [duration])
+
 
   return (
+    <>
+      {!timeUp && <h1 id="counter">{timer}</h1>}
+      { timeUp && <h2 id="counter">Times Up!</h2>}
+      <div className="item">
+        <div className="image">
+          <Image className="image" src={questionImg} size='big' />
+        </div>
+        <div className="content">
+          <h2 className="header">{props.question.questionHeader}</h2>
+          <h3 className="description">{props.question.questionText}</h3>
+          <Divider hidden />
+          <div className="ui input"><input type="text" value={props.inputText} onChange={e => props.handleInput(e.target.value)} placeholder="Enter the answer" /></div>
+          <Divider hidden />
+          
+          <button className="ui primary button" onClick={() => { props.handleSubmitAnswer() }}>Submit</button>
 
-    <div className="item">
-      <div className="image">
-        <Image className="image" size='small' />
+          <Modal
+            centered={false}
+            open={clue}
+            onClose={() => openClue(false)}
+            onOpen={() => openClue(true)}
+            trigger={<Button className="ui secondary button">Show Clue</Button>}
+
+          >
+            <Modal.Header>Here is your clue. Best luck!</Modal.Header>
+            <Modal.Content>
+              <Modal.Description>
+                {props.question.clue}
+              </Modal.Description>
+            </Modal.Content>
+            <Modal.Actions>
+              <Button onClick={() => openClue(false)}>OK</Button>
+            </Modal.Actions>
+          </Modal>
+        </div>
+
       </div>
-      <div className="content">
-        <h2 className="header">{props.question.questionHeader}</h2>
-        <div className="description">{props.question.questionText}</div>
-        <Divider hidden />
-        <div className="ui input"><input type="text" value={props.inputText} onChange={e => props.handleInput(e.target.value)} placeholder="Enter the answer" /></div>
-        <Divider hidden />
-        <button className="ui primary button" onClick={() => { props.handleSubmitAnswer() }}>Submit</button>
-
-        <Modal
-          centered={false}
-          open={clue}
-          onClose={() => openClue(false)}
-          onOpen={() => openClue(true)}
-          trigger={<Button className="ui secondary button">Show Clue</Button>}
-
-        >
-          <Modal.Header>Here is your clue. Best luck!</Modal.Header>
-          <Modal.Content>
-            <Modal.Description>
-              {props.question.clue}
-            </Modal.Description>
-          </Modal.Content>
-          <Modal.Actions>
-            <Button onClick={() => openClue(false)}>OK</Button>
-          </Modal.Actions>
-        </Modal>
-      </div>
-
-    </div>
+    </>
   )
 
 }
